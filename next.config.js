936f53161f9c820dd45d229ea8ad4e4929365c3e
atshushi <atsushithesushi@gmail.com>
2023-03-05 19:00:01 -0300

    Remove Esbuild

--- next.config.js
+++ next.config.js
@@ -1,20 +1,5 @@
-const { ESBuildPlugin } = require('esbuild-loader');
-
 module.exports = {
   experimental: {
     appDir: true,
-  },
-  webpack: (config, { webpack, dev }) => {
-    if (!dev) {
-      config.plugins.push(
-        new webpack.ProvidePlugin({
-          React: 'react',
-        }),
-      );
-
-      config.plugins.push(new ESBuildPlugin());
-    }
-
-    return config;
   },
 };
